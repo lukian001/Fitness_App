d9b48ac6f12ea4c4fa0adee6c41fc6f9d785bdd8
lukian001 <lucian.streulea02@gmail.com>
2023-08-31 00:54:45 +0300

    started adding step counter

--- app/src/main/java/com/licenta/fitnessapp/ui/composables/StepCounterMenu.kt
+++ app/src/main/java/com/licenta/fitnessapp/ui/composables/StepCounterMenu.kt
@@ -1,20 +1,29 @@
 package com.licenta.fitnessapp.ui.composables
 
+import android.os.Build
 import android.widget.Button
 import android.widget.Space
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.licenta.fitnessapp.logic.StepCounterManager
+import java.time.LocalDateTime
 
 object StepCounterMenu: Menu() {
+    @RequiresApi(Build.VERSION_CODES.O)
     @Composable
     override fun CustomUi() {
         Box(
@@ -23,9 +32,26 @@ object StepCounterMenu: Menu() {
             Column(
                 modifier = Modifier.fillMaxSize()
             ) {
-                Text("Current steps ${StepCounterManager.currentSteps.value}")
+                Spacer(Modifier.weight(0.2f))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = LocalDateTime.now().toString(),
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp
+                )
+                Spacer(Modifier.weight(0.5f))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Current Walked Steps: 3",
+                    textAlign = TextAlign.Center,
+                    fontSize = 24.sp
+                )
+
                 Spacer(Modifier.weight(1f))
-                Button(onClick = {
+                Button(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(5.dp),
+                    onClick = {
                     if (!StepCounterManager.running.value) {
                         StepCounterManager.onResume()
                     } else {
@@ -33,7 +59,7 @@ object StepCounterMenu: Menu() {
                     }
                 }) {
                     if (!StepCounterManager.running.value) {
-                        Text("Start step counter")
+                        Text("Stop step counter")
                     } else {
                         Text("Stop step counter")
                     }
